ee0aa3261067db82e2fee076fc7ba622d524224a
nakako <nakako.soft@gmail.com>
2021-02-28 22:33:17 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 'use strict';
+const memo = new Map();
+// n=0, n=1は決まっているのでsetしておく
+memo.set(0, 0);
+memo.set(1, 1);
+
+
 function fib(n){
-    if((n === 0) || (n === 1)){
-        return n;
+    // memoにデータがあればそれを使う
+    if(memo.has(n)){
+        return memo.get(n);
     }
+    // memoにデータがないので計算する
     else{
-        return fib(n-1)+fib(n-2);
+        let value = fib(n-1) + fib(n-2);
+        memo.set(n, value);
+        return value;
     }
 }
 
 const number = process.argv[2] || 0;　// コンソールから引数を持ってくる
-for(let i = 1 ; i <= number ; i++){
+for(let i = 0 ; i <= number ; i++){
     console.log(`${i} のフィボナッチ数　：　${fib(i)}`);
+    console.log(memo);
+    console.log('-----------------------------------');
 }
