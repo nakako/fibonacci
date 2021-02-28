67251ad61449414f6eec9b17d43caffbf97cab4e
nakako <nakako.soft@gmail.com>
2021-02-28 21:44:28 +0900

    指数オーダのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+function fib(n){
+    if((n === 0) || (n === 1)){
+        return n;
+    }
+    else{
+        return fib(n-1)+fib(n-2);
+    }
+}
+
+const number = process.argv[2] || 0;　// コンソールから引数を持ってくる
+for(let i = 1 ; i <= number ; i++){
+    console.log(`${i} のフィボナッチ数　：　${fib(i)}`);
+}
